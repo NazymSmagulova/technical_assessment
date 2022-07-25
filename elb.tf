9f60d019daca7fb1065db6f00a69cd1cc75bfe67
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 19:24:32 -0400

    added subnets for elb

--- elb.tf
+++ elb.tf
@@ -76,7 +76,7 @@ resource "aws_elb" "example" {
   name               = "terraform-asg-example"
   security_groups    = [aws_security_group.elb.id]
   availability_zones = data.aws_availability_zones.all.names
-
+  subnets = [aws_subnet.public1.id, aws_subnet.public2.id]
 
   health_check {
     target              = "HTTP:${var.server_port}/"
