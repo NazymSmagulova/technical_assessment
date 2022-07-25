f1e03d9c55acecf3caa0d957515f27ccf32d037f
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 17:38:51 -0400

    added backend

--- backend.tf
+++ backend.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket" "main" {
   bucket_prefix = "my-project-"
   acl           = "private"
 }
+
+
+
+ terraform {
+   backend "s3" {
+     bucket = "terraform-s3-backend-test-assessment"
+     key    = "us-east-2/test"
+     region = "us-east-2"
+   }
+ }
