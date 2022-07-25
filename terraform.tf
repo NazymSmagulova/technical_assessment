7b7e108a11cfa1561db8025e130baa72304928d4
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 17:50:18 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
+ terraform {
+    backend "s3" {
+      bucket = "terraform-s3-backend-test-assessment"
+      key    = "us-east-2/test"
+      region = "us-east-2"
     }
   }
