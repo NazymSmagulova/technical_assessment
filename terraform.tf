2c23681ae962396645a12fc3e0a9c6eb23c740b2
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 17:42:47 -0400

    bckt name

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
     backend "s3" {
-    bucket = "terraform-s3-backend-pmh86b2v"
+    bucket = "terraform-s3-backend-test-assessment"
     region = "ap-northeast-1"
     key    = "terraform.tfstate"
   }
