882582d9c887008bd9475a51eecba17b73dc6806
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 16:04:25 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,16 +2,26 @@
 # ------------------------------------------------------------------------------
 # CREATE THE S3 BUCKET
 # ------------------------------------------------------------------------------
+resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new s3 bucket
+    aws_s3_bucket = var.aws_s3_bucket
+  }
+}
 
+resource "aws_instance" "server" {
+  tags = {
+    Name = "web-server-${random_pet.server.id}"
+  }
 resource "aws_s3_bucket" "terraform_state" {
-  # TODO: change this to your own name! S3 bucket names must be *globally* unique.
-  bucket_prefix = "my-backend-bucket-"
-  bucket = "nazyma"
     acl    = "private"
   # Enable versioning so we can see the full revision history of our
   # state files
   versioning {
     enabled = true
+  }
+    tags = {
+    Name = "web-server-${random_pet.server.id}"
   }
 
   # Enable server-side encryption by default
