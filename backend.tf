22c06150bd82c8b90354cd8e8a812083f4793304
NazymSmagulova <96206407+NazymSmagulova@users.noreply.github.com>
2022-07-25 17:49:20 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,7 @@
-resource "aws_s3_bucket" "backend" {
-  bucket = "terraform-s3-backend-test-assessment"
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "main" {
-  bucket_prefix = "my-project-"
-  acl           = "private"
-}
-
-
-    backend "s3" {
-    bucket = "terraform-s3-backend-test-assessment"
-    region = "us-east-1"
-    key    = "terraform.tfstate"
-  }
-}
+ terraform {
+   backend "s3" {
+     bucket = "technicalassessment-nazyma"
+     key    = "us-east-2/test"
+     region = "us-east-2"
+   }
+ }
